a52c0c956546ac6ad4944fe9049e1ce7d3f30f18
vsamaru <vsamaru@gmail.com>
2021-01-04 20:53:38 +0200

    Add files via upload

---
+++ precache-manifest.9414c1ad04d6cc25138d15287f018c90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80f894cbbd94e08e89cde9a2a92a0bd",
+    "url": "/tailwind-gallery/index.html"
+  },
+  {
+    "revision": "f897fb84a401dd59edc9",
+    "url": "/tailwind-gallery/static/css/main.3075c778.chunk.css"
+  },
+  {
+    "revision": "5c45fef4dbb3cf765ad3",
+    "url": "/tailwind-gallery/static/js/2.e7ed26ff.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tailwind-gallery/static/js/2.e7ed26ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f897fb84a401dd59edc9",
+    "url": "/tailwind-gallery/static/js/main.362996ce.chunk.js"
+  },
+  {
+    "revision": "1bc71378144d6744c618",
+    "url": "/tailwind-gallery/static/js/runtime-main.569aac49.js"
+  }
+]);
